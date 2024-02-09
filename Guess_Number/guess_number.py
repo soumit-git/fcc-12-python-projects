95fc05e9e8ec1b9388df7fcb9442a229382bced9
Soumit Goswami <soumit.goswami12@gmail.com>
2024-02-09 12:30:47 +0530

    Add computer part of guessing game

--- Guess_Number/guess_number.py
+++ Guess_Number/guess_number.py
@@ -11,4 +11,23 @@ def guess(x):
       print("Sorry, guess again. Too low!")
   print(f"Yay, congrats. You have guess {random_number} correctly!")
 
-guess(10)
+def computer_guess(x):
+  low = 1
+  high = x
+  feedback = ''
+
+  while feedback != 'c':
+    if low != high:
+      guess = random.randint(low, high)
+    else:
+      guess = low
+    
+    feedback = input(f"Is {guess} too High (H), too Low (L) or Correct (C)?").lower()
+    if feedback == "h":
+      high = guess - 1
+    elif feedback == "l":
+      low = guess + 1
+  print(f"Yay! The computer guessed your number, {x}, correclty!")
+
+# guess(10)
+computer_guess(10)
